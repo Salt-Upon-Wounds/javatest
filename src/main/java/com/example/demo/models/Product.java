4b48d824a245fd61ae672cefd2894e6bb69b169b
Kirill <kiw197204@gmail.com>
2022-12-14 13:27:45 +0300

    init: started project

---
+++ src/main/java/com/example/demo/models/Product.java
@@ -0,0 +1,14 @@
+package com.example.demo.models;
+
+public class Product {
+    private int id;
+    private float cost;
+    private String name;
+    public Product(int id, float cost, String name) {
+        this.id = id;
+        this.cost = cost;
+        this.name = name;
+    }
+
+
+}
